e8619787a0a57f52bff4eefb93cc6fd01657b24a
Robert Stefaniak <robertostefaniak@googlemail.com>
2020-01-03 19:43:34 +0100

    Zmienne typu referencyjnego (zlozonego) sa wskaznikami do obiektow!

---
+++ Lekcja10/Diary.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja10
+{
+    class Diary
+    {
+        // Stan. Wprowadzanie ocen do listy
+
+        List<float> ratings = new List<float>();
+
+        // Metody. Obliczanie sredniej ocen. Wyznaczanie najwiekszej i najmniejszej oceny.
+
+        public void AddRating(float rating)
+        {
+            //float rating;
+            //int count_ratings;
+            //Console.Write("Ile ocen chcesz wprowadzic do dzienniczka: ");
+            //count_ratings = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < count_ratings; i++)
+
+            //for (; ; )
+            //{
+            //    Console.Write("Dodaj ocene do dzienniczka: ");
+            //    bool result = float.TryParse(Console.ReadLine(), out rating);
+
+            //    if (result == false)
+            //    {
+            //        Console.WriteLine("Nie podales oceny!");
+            //    }
+            //    else if (result)
+            //    {
+            //        if (rating == 0)
+            //            break;
+            //        else if (rating >= 1 && rating <= 6)
+            //        {
+            //            ratings.Add(rating);
+            //        }
+            //        else
+            //            Console.WriteLine("Podales ocene spoza przedzialu!");
+            //    }
+            //}
+            ratings.Add(rating);
+        }
+
+        public void ShowRaitings()
+        {
+            foreach (var item in ratings)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            return sum / ratings.Count();
+        }
+
+        public float MaxRating()
+        {
+
+            return ratings.Max();
+        }
+
+        public float MinRating()
+        {
+
+            return ratings.Min();
+        }
+    }
+}
---
+++ Lekcja10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zmienne typu referencyjnego sa wskaznikami do obiektow!
+
+            Diary diary = new Diary();
+            diary.AddRating(4);
+            diary.AddRating(4);
+            Diary diary1 = diary;
+            diary1.AddRating(5);
+            Diary diary2 = diary;
+            diary2.AddRating(3);
+
+            diary2.ShowRaitings();
+            Console.WriteLine("Najwyzsza osena: " + diary1.MaxRating());
+            Console.WriteLine("Najnizsza osena: " + diary.MinRating());
+            Console.WriteLine("Srednia ocen = " + diary2.CalculateAverage());
+
+            Console.ReadKey();
+        }
+    }
+}
